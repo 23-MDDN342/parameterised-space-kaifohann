a887296d78d50dbe7ac9e959764d4d07ac328bbc
kaifohann <kaifohann@myvuw.ac.nz>
2024-03-21 14:20:21 +1300

    Last update on README before final. Cleaned and commented code

--- README.md
+++ README.md
@@ -18,8 +18,7 @@ https://stackoverflow.com/questions/67634103/how-to-get-a-top-view-house-in-p5-j
 
 
 colour shifting:
-My first sketch is based off of this gif:
-https://bestanimations.com/Balls&Buttons/amazing-3d-computer-ball-sphere-art-animated-gif-6.gif
+My first sketch is based off of [this gif] (https://bestanimations.com/Balls&Buttons/amazing-3d-computer-ball-sphere-art-animated-gif-6.gif):
 
 I find this animation completely mesmerising and would love to see how I could expand on it. For my sketch I have turned the basic sphere shape into a pattern as I think this will look more satisfying in wallpaper format. I have left the sketch as black and white for now because I'd like to test a few options in combination with the animation effect. To distinguish my own work from the original gif, I'd quite like to work with a shift between 2 colours rather than a whole spectrum, but the effect of this needs to be tested in animation.
 
@@ -39,4 +38,7 @@ After getting the colour changing cycle working I tried a few different colour c
 I've now also managed to fit the spheres into a grid pattern using for loops. I've tested how the canvas looks depending on how many rows/columns I drew. I also played around with layering the spheres. Currently the code is drawing 2 grids - one with lots of smaller spheres in the background and another with larger,fewer spheres in the foreground. 
 
 UPDATE 4:
-To add some interest I've experimented with side scroll. I used the example_sidescroll.js given to us for the set up of my code. I've experimented with just big spheres, only smaller spheres and both together. A happy accident was making the two types of spheres move in opposite directions. This created a crossing effect where the smaller ones would start in the background and end up in the foreground. The dynamic look this created resonated with me so I'm going to see where it leads. As there's a bit of extra white space, I'm currently looking at how many smaller/bigger spheres should be placed to appropriately fill the canvas during movement.
+To add some interest I've experimented with side scroll. I used the example_sidescroll.js given to us for the set up of my code. I've experimented with just big spheres, only smaller spheres and both together. A happy accident was making the two types of spheres move in opposite directions. This created a crossing effect where the smaller ones would start in the background and end up in the foreground. The dynamic look this created appeals to me so I'm going to see where it leads. As there's a bit of extra white space, I'm currently looking at how many smaller/bigger spheres should be placed to appropriately fill the canvas during movement.
+
+UPDATE 5: 
+Refining time - I settled at the crossing movement that I tested out previously. I experimented with a few differnt sizes of the spheres, but ultimately decided the original was more balanced. I'm really happy with the effect of this. I played around with a few different colour palettes, looking at different blues and greens in particular, but again, I decided to come back to my first iteration. (The accidental) 70s colour palette that I stumbled onto feels right with the sphere shape to me. Probably because of its similarity to 70s flower power patterns [like this] (https://carmengonzalezartist.com/product/delicate-flower-power-20x20-acrylic-on-canvas-original/) that I've always had an affinity with. I think the general visual feel of the final has some quite clear ties to other 70s inspiration, specifically the 'trippy' [psychedelic aesthetic] (https://www.pinterest.com/vivianic/psychedelic/) that was so popular.
--- draw_one_frame.js
+++ draw_one_frame.js
@@ -1,34 +1,17 @@
-//var x = 300;
-//var y = 300;
-//var a = 100;
-//var b = 100;
-
-//var CoordX1 = 0;//middle circle coord
-//var CoordY1 = 0;
-
-// this is the fireworks example
-//https://editor.p5js.org/anjchang/sketches/vL3ul5F73 maybe help with size changing
-
-
 function draw_one_frame(cur_frac) {
 	background('white');
 	angleMode(DEGREES);
-	//translate(width/2, height/2); //draw from center
+	
 	var circleSize = height/2.5; //establish circle size
 	noStroke();
-	GridDraw(cur_frac, circleSize);
+	GridDraw(cur_frac, circleSize); //call function that draws the grid of circles
 	
 }	
 
 function GridDraw(cur_frac, circleSize){
 
-	//for (let X = 0; X<=width; X+= width/8) {
-		//for (let Y=0; Y<= height; Y+= height/4) {
-			//drawSphere(cur_frac, X, Y, circleSize/1.85);	
-		//}
-	//}
+	//establish what points each set of circles passes through
 	let grid_points1 = [ 
-		// 1.25 * width, 0.75 * width,0.50 * width,0.25 * width, -0.25 * width,-0.50 * width
 		-0.25 * width,
     	0.0 * width,
     	0.25 * width,
@@ -50,8 +33,6 @@ function GridDraw(cur_frac, circleSize){
 		-width, 
 		-1.375*width
 	]
-
-
 	let grid_points3 = [
 		1.25 * width, 
 		1.00 * width,
@@ -60,66 +41,48 @@ function GridDraw(cur_frac, circleSize){
 		0.25 * width,
 		0.0 * width,
 		-0.25 * width
-    	
-    
 	]
 
+	//side scroll for loop. Sections of code taken from example_sidescroll.js
 	for(let i=0; i<grid_points1.length-1; i++) {
+
+		//establish movement maps for each circle set
 		let cur_x_pos = map(cur_frac, 0, 1, grid_points1[i], grid_points1[i+1])
 		let cur_x_pos2 = map(cur_frac, 0, 1, grid_points2[i], grid_points2[i+1])
 		let cur_x_pos3 = map(cur_frac, 0, 1, grid_points3[i], grid_points3[i+1])
 
-		//for (let X = 0; X<=width; X+= width/8) {
-			//for (let Y=0; Y<= height; Y+= height/4) {
-				drawSphere(cur_frac, cur_x_pos3, height*0.5, circleSize/1.85);
-				drawSphere(cur_frac, cur_x_pos3, height*0.8, circleSize/1.85);
-				drawSphere(cur_frac, cur_x_pos3, height*0.2, circleSize/1.85);
+
+		drawSphere(cur_frac, cur_x_pos3, height*0.5, circleSize/1.85);
+		drawSphere(cur_frac, cur_x_pos3, height*0.8, circleSize/1.85);
+		drawSphere(cur_frac, cur_x_pos3, height*0.2, circleSize/1.85);
 
 
-				drawSphere(cur_frac, cur_x_pos2, height/height, circleSize/1.85);	
-				drawSphere(cur_frac, cur_x_pos2, height*0.33, circleSize/1.85);	
-				drawSphere(cur_frac, cur_x_pos2, height*0.67, circleSize/1.85);	
-				drawSphere(cur_frac, cur_x_pos2, height, circleSize/1.85);	
-
-					
+		drawSphere(cur_frac, cur_x_pos2, height/height, circleSize/1.85);	
+		drawSphere(cur_frac, cur_x_pos2, height*0.33, circleSize/1.85);	
+		drawSphere(cur_frac, cur_x_pos2, height*0.67, circleSize/1.85);	
+		drawSphere(cur_frac, cur_x_pos2, height, circleSize/1.85);	
+	
 						
+		//draw the big circles in reverse 
+		REVdrawSphere(cur_frac, cur_x_pos, height/2, circleSize);	
+		REVdrawSphere(cur_frac, cur_x_pos, height/11, circleSize);	
+		REVdrawSphere(cur_frac, cur_x_pos, height/1.1, circleSize);
 
-			//}
-		//}
-		//for (let X = 0; X<=width; X+= width/4) {
-			//for (let Y=0; Y<= height; Y+= height/2) {
-		
-				REVdrawSphere(cur_frac, cur_x_pos, height/2, circleSize);	
-				REVdrawSphere(cur_frac, cur_x_pos, height/11, circleSize);	
-				REVdrawSphere(cur_frac, cur_x_pos, height/1.1, circleSize);
-	 	//}
-		
-		//}
 	}
-	//for (let X = 0; X<=width; X+= width/4) {
-		//for (let Y=0; Y<= height; Y+= height/2) {
-			//drawSphere(cur_frac, X, Y, circleSize);	
-		//}
-	//}
 
 	
 }
 
 
-function BGDraw (cur_frac, CoordX1, CoordY1, circleSize) {
 
-	//const ellipArray = ['#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233','#4a2a24', '#24171e'];
-	//let colourMap =int(map(cur_frac, 0, 1, 0,ellipArray.length))
-	//fill(ellipArray[colourMap])
 
-	//ellipse(CoordX1, CoordY1, circleSize*1.1, circleSize*1.1)
-}
-
+//Draw sphere (colours spinning right to left)
 function drawSphere(cur_frac, CoordX1, CoordY1, circleSize) {
 
-	var innerCircleSize = circleSize/1.5;
-	//pick 12 colours for final 
-	//maybe turn this into a nested array???
+	//establish size of internal circle in sphere
+	var innerCircleSize = circleSize/1.5; 
+	
+	//establish colour arrays for each section. These have to be staggered for the effect to work
 	const fill_Array = ['#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233','#4a2a24', '#24171e'];
 	const fill_Array1 = ['#24171e','#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233','#4a2a24'];
 	const fill_Array2 = ['#4a2a24','#24171e','#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233'];
@@ -134,13 +97,11 @@ function drawSphere(cur_frac, CoordX1, CoordY1, circleSize) {
 	const fill_Array11 = ['#a83b68','#c56161','#ea9854','#f6cda8','#fbfcf5','#d6dd9f','#aebc43','#a2863c','#955233','#4a2a24','#24171e','#0f101c','#351a2f'];
 	const fill_Array12 = ['#351a2f','#a83b68','#c56161','#ea9854','#f6cda8','#fbfcf5','#d6dd9f','#aebc43','#a2863c','#955233','#4a2a24','#24171e','#0f101c'];
 
-	
+	//map colour arrays to cur_frac
 	let colourMap =int(map(cur_frac, 0, 1, 0,fill_Array.length))
 
-	
-
+	//Draw the sphere in sections using a for loop
 	for (let i = 0; i < 13; i ++) {
-		
 		if (i == 0) {
 			fill(fill_Array[colourMap]);
 			arc(CoordX1, CoordY1, circleSize+circleSize/30, circleSize, 270, 90);
@@ -208,14 +169,16 @@ function drawSphere(cur_frac, CoordX1, CoordY1, circleSize) {
 			arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.2), innerCircleSize, 90, 270);
 		}
 	
-}
+	}
 }
 
+//Duplicate function, drawing spheres with a reverse colour palette - giving the impression of the sphere spinning from left to right
 function REVdrawSphere(cur_frac, CoordX1, CoordY1, circleSize) {
 
+	//establish size of internal circle in sphere
 	var innerCircleSize = circleSize/1.5;
-	//pick 12 colours for final 
-	//maybe turn this into a nested array???
+	
+	//establish colour arrays for each section. These have to be staggered for the effect to work
 	const fill_Array12 = ['#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233','#4a2a24', '#24171e'];
 	const fill_Array11 = ['#24171e','#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233','#4a2a24'];
 	const fill_Array10 = ['#4a2a24','#24171e','#0f101c','#351a2f', '#a83b68','#c56161', '#ea9854', '#f6cda8','#fbfcf5', '#d6dd9f', '#aebc43','#a2863c','#955233'];
@@ -230,13 +193,12 @@ function REVdrawSphere(cur_frac, CoordX1, CoordY1, circleSize) {
 	const fill_Array1 = ['#a83b68','#c56161','#ea9854','#f6cda8','#fbfcf5','#d6dd9f','#aebc43','#a2863c','#955233','#4a2a24','#24171e','#0f101c','#351a2f'];
 	const fill_Array = ['#351a2f','#a83b68','#c56161','#ea9854','#f6cda8','#fbfcf5','#d6dd9f','#aebc43','#a2863c','#955233','#4a2a24','#24171e','#0f101c'];
 
-	
+	//map colour arrays to cur_frac
 	let colourMap =int(map(cur_frac, 0, 1, 0,fill_Array.length))
 
 	
-
+	//Draw the sphere in sections using a for loop
 	for (let i = 0; i < 13; i ++) {
-		
 		if (i == 0) {
 			fill(fill_Array[colourMap]);
 			arc(CoordX1, CoordY1, circleSize+circleSize/30, circleSize, 270, 90);
@@ -303,205 +265,5 @@ function REVdrawSphere(cur_frac, CoordX1, CoordY1, circleSize) {
 			fill(fill_Array[colourMap]);
 			arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.2), innerCircleSize, 90, 270);
 		}
-	
-}
-
-}
-
-
-/*
-for (let i = 0; i < 13; i ++) {
-		
-	let colourMap =int(map(cur_frac, 0, 1, 0,fill_Array.length))
-
-	if (i == 0) {
-		fill(fill_Array12[colourMap]);
-		arc(CoordX1, CoordY1, circleSize+circleSize/30, circleSize, 90, 270);
-	} else if (i == 1) {
-		fill(fill_Array11[colourMap]);
-		arc(CoordX1-circleSize/5.2, CoordY1, circleSize/1.55, circleSize/1.2, 60, 300);
-	} else if (i == 2) {
-		fill(fill_Array10[colourMap]);
-		arc(CoordX1-circleSize/6.2, CoordY1, circleSize/1.5, circleSize/1.3, 65, 295);
-	} else if (i == 3) {
-		fill(fill_Array9[colourMap]);
-		arc( CoordX1-circleSize/8.2, CoordY1, circleSize/1.45, circleSize/1.4, 70, 290);
-	} else if (i == 4) {
-		fill(fill_Array8[colourMap]);
-		arc(CoordX1-circleSize/12,CoordY1,circleSize/1.45, circleSize/1.46, 80, 280);
-	} else if (i == 5) {	
-		fill(fill_Array7[colourMap]);
-		arc(CoordX1-circleSize/22,CoordY1,circleSize/1.5, circleSize/1.48, 83, 277);
-	} else if (i == 6){
-		fill(fill_Array6[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize, innerCircleSize, 90, 270);
-	} else if (i == 7) {
-		fill(fill_Array5[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/9.9), innerCircleSize, 90, 270);	
-	} else if (i == 8) {	
-		fill(fill_Array4[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/4.9), innerCircleSize, 90, 270);
-	}else if (i == 9) {	
-		fill(fill_Array3[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/3), innerCircleSize, 90, 270);
-	}else if (i == 10) {
-		fill(fill_Array2[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/2), innerCircleSize, 90, 270);
-	}else if (i == 11) {	
-		fill(fill_Array1[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.5), innerCircleSize,  90,270);
-	}else if (i == 12) {	
-		fill(fill_Array[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.2), innerCircleSize, 90, 270);
-	}
-
-}*/
-
-/*
-for (let i = 0; i < 9; i ++) {
-		
-	let colourMap =int(map(cur_frac, 0, 1, 0,fill_Array.length))
-
-	if (i == 0) {
-		fill(fill_Array8[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize+(innerCircleSize/5.5), innerCircleSize, 90, 270);
-	} else if (i == 1) {	
-		fill(fill_Array7[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize+(innerCircleSize/10), innerCircleSize, 90, 270);
-	} else if (i == 2){
-		fill(fill_Array6[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize, innerCircleSize, 90, 270);
-	} else if (i == 3) {
-		fill(fill_Array5[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/9.9), innerCircleSize, 90, 270);	
-	} else if (i == 4) {	
-		fill(fill_Array4[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/4.9), innerCircleSize, 90, 270);
-	}else if (i == 5) {	
-		fill(fill_Array3[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/3), innerCircleSize, 90, 270);
-	}else if (i == 6) {
-		fill(fill_Array2[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/2), innerCircleSize, 90, 270);
-	}else if (i == 7) {	
-		fill(fill_Array1[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.5), innerCircleSize, 90, 270);
-	}else if (i == 8) {	
-		fill(fill_Array[colourMap]);
-		arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.2), innerCircleSize, 90, 270);
-	}
-
-}
-
-
-*/
-
-/*
-	//LEFT SIDE 
-	for (let i = 0; i < 9; i ++) {
-		
-		const fill_Array = ['#FF0000', '#FF7F00','#FFFF00','#00FF00','#0000FF','#4B0082', '#9400D3', 'brown', 'white'];
-		
-		fill(fill_Array[i]);
-
-		if (i < 2){
-			arc(CoordX1,CoordY1,innerCircleSize+(innerCircleSize/sizeAdjust_Array[i]), innerCircleSize, 90, 270);
-			
-		} else if (i == 2) {
-		
-			arc(CoordX1,CoordY1,innerCircleSize, innerCircleSize, 90, 270);
-		} else {
-
-	
-			arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/sizeAdjust_Array[i]), innerCircleSize, 90, 270);
-		}
-	}
-
-*/
-
-	
-/*
-	//right to left inner circle
-	fill('#9400D3');//light purp
-	arc(CoordX1,CoordY1,innerCircleSize, innerCircleSize, 270, 90);
-	fill('#4B0082');//dark purp
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/9.9), innerCircleSize, 270, 90);
-	fill('#0000FF');//blue
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/4.9), innerCircleSize, 270, 90);
-	fill('#00FF00');//green
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/3), innerCircleSize, 270, 90);
-	fill('#FFFF00');//yellow
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/2), innerCircleSize, 270, 90);
-	fill('#FF7F00');//orange
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.5), innerCircleSize, 270, 90);
-	fill('#FF0000');//red
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.2), innerCircleSize, 270, 90);
-
-
-	fill('#FF0000');//light purp
-	arc(CoordX1,CoordY1,innerCircleSize, innerCircleSize, 90, 270);
-	fill('#FF7F00');//dark purp
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/9.9), innerCircleSize, 90, 270);
-	fill('#FFFF00');//blue
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/4.9), innerCircleSize, 90, 270);
-	fill('#00FF00');//green
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/3), innerCircleSize, 90, 270);
-	fill('#0000FF');//yellow
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/2), innerCircleSize, 90, 270);
-	fill('#4B0082');//orange
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.5), innerCircleSize, 90, 270);
-	fill('#9400D3');//red
-	arc(CoordX1,CoordY1,innerCircleSize-(innerCircleSize/1.2), innerCircleSize, 90, 270);
-	
-*/
-	
-	
-	//beginShape();
-	//vertex(CoordX1+(circleSize/2), CoordY1+(circleSize/2));
-	//bezierVertex(40,60, 30, 80, 90, 20);
-	//vertex(CoordX1-(circleSize/2), CoordY1-(circleSize/2));
-	//endShape(CLOSE);
-
-	//background(255);
-	//x += 2;
-	//y += 2;
-	//a -= 2;
-	//b -= 2;
-	//strokeWeight(1);
-	//translate(width / 2, height / 2);
-	//for (var i = 0; i < 15; i++) {
-		//for (var k = 0; k < 20; k++) {
-			//stroke(255, 255, 255);
-			//rotate(PI / 12.0);
-			//fill(255, 255 - i * 10, 255 - k * 10);
-			//line(a % 100, b % 100, x % 300, y % 300);
-			//ellipse((x + i * 20) % width, (y + k * 20) % height, i + 4, i + 4);
-			//drawtriangle((a - i * 20) % width, (b - k * 20) % height, k / 8);
-			//rect(x % width, y % height, k + 10, k + 10);
-			//fill(0, i * 10, 255 - k * 10);
-			//ellipse((x - i * 20) % width, (y - k * 20) % height, i + 3, i + 3);
-			//rotate(PI / 24.0);
-			//fill(255 - (i + k) * 5, (i + k) * 7, i * 20);
-			//drawtriangle((a + i * 20) % width, (b + k * 20) % height, k / 8);
-			//rect(a % width, b % height, k + 5, k + 5);
-			//drawflower(k, x);
-		//}
-	//}
-
-//}
-
-function drawtriangle(x, y, r) {
-	triangle(x, y, x + 7 * r, y - 13.75 * r, x + 14 * r, y);
-}
-
-function drawflower(i, k) {
-	if (i % 2 == 1) {
-		fill(255, (k * 0.4) % 255, 30);
-		stroke(k % 255, 255, 0);
-		arc(0, 0, 150 + i + 150 * sin(k * PI / 24), 150, 0, PI / 40);
-	} else {
-		fill(k % 255, 0, 255);
-		stroke(0, (k * 0.4) % 255, 255);
-		arc(0, 0, (100 + 100 * cos(k * PI / 24)) % 255, 50, 0, PI / 20);
 	}
 }
